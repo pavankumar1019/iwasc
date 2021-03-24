501c02949f46955d69a29f6ece89c5dc1b9fd564
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-03-24 15:22:00 +0530

    a

--- index.php
+++ index.php
@@ -371,12 +371,10 @@ $(document).ready(function(){
   $('#import_excel_form').on('submit', function(event){
     event.preventDefault();
     $.ajax({
-      url:"import.php",
-      method:"POST",
-      data:new FormData(this),
-      contentType:false,
-      cache:false,
-      processData:false,
+      url: "import.php",
+type: "POST",
+data: new FormData(this),
+contentType: false, cache: false, processData:false,
       beforeSend:function(){
         // $('#import').attr('disabled', 'disabled');
         $('#import').val('Importing...');
