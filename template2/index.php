1379546e79b2f299baf575dbffa1abdcb146ae7b
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-03-26 13:24:30 +0530

    a

--- template2/index.php
+++ template2/index.php
@@ -18,6 +18,10 @@ $generator = new \Picqer\Barcode\BarcodeGeneratorPNG();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
         crossorigin="anonymous"></script>
+        <style>
+        p{
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}</style>
 </head>
 <body>
 <?php
