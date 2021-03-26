b6a981f7c4f7e88c28ca447c897a762195d6e233
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-03-26 13:28:51 +0530

    a

--- template2/index.php
+++ template2/index.php
@@ -21,7 +21,7 @@ $generator = new \Picqer\Barcode\BarcodeGeneratorPNG();
         <style>
         p{
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    font-weight: 900;
+    font-weight: 600;
 }</style>
 </head>
 <body>
