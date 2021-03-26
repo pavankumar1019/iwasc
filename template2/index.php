115e468a364d0932edd7042877527dc287357129
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-03-26 14:12:02 +0530

    a

--- template2/index.php
+++ template2/index.php
@@ -68,7 +68,7 @@ if ($result = mysqli_query($con, $sql)) {
             <div class="col" style="display: flex;flex-direction: column; -ms-flex-wrap: wrap;
             flex-wrap: wrap;align-items: center;justify-content: center;">
             <div style="text-align: center;">
-                <img src="./logo.png" width="100px" class="img-fluid" alt=""><br>
+                <img src="./logo.png" width="250px" class="img-fluid" alt=""><br>
                 <img src="./principal.png" width="100px" alt="">
                 <h2>PRINCIPAL</h2>
             </div>
