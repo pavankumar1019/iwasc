3da4a93a913fdad6ee57a4ebd8af374e6d83cfa5
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-03-26 14:16:44 +0530

    a

--- template2/index.php
+++ template2/index.php
@@ -52,7 +52,7 @@ if ($result = mysqli_query($con, $sql)) {
             <img src="./Group1.png" class="img-fluid" alt="" srcset="" />
         </div>
     </div>
-    <div style="margin-top: -100px;">
+    <div style="margin-top: -110px;">
     <div style="text-align: left;margin-left:50px;">
     <h1 style="
     font-weight: 700; text-transform: uppercase;"> <?php echo $fullname;?>
